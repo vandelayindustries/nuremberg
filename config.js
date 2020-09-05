4c6e78271cf1f69a97c8b1acd2f883e7e64d1968
Des Preston <despreston@gmail.com>
2020-09-05 17:47:48 -0400

    stop creating new meta docs during testing.
    most insecure: use average edits per message

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 const config = {
-  'dev': {
-    'slackResultsChannel': process.env.slack_results_channel,
-    'slackToken': process.env.slack_token,
-    'db': {
-      'conn': 'mongodb://localhost:27017',
-      'name': 'vandelay-industries'
+  dev: {
+    slackResultsChannel: process.env.slack_results_channel,
+    slackToken: process.env.slack_token,
+    saveMeta: process.env.SAVE_META || false,
+    db: {
+      conn: 'mongodb://localhost:27017',
+      name: 'vandelay-industries'
     }
   },
-  'prod': {
-    'slackResultsChannel': process.env.slack_results_channel,
-    'slackToken': process.env.slack_token,
-    'db': {
-      'conn': `mongodb+srv://${ process.env.db_username }:${ process.env.db_password }@cluster0-ltj7y.mongodb.net/test?retryWrites=true&w=majority`,
-      'name': 'vandelay-industries'
+  prod: {
+    slackResultsChannel: process.env.slack_results_channel,
+    slackToken: process.env.slack_token,
+    saveMeta: true,
+    db: {
+      conn: `mongodb+srv://${ process.env.db_username }:${ process.env.db_password }@cluster0-ltj7y.mongodb.net/test?retryWrites=true&w=majority`,
+      name: 'vandelay-industries'
     }
   }
 };
