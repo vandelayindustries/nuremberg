5dbc782fbf02709e838c7d663d9cfb24e050ec13
Des Preston <despreston@gmail.com>
2021-01-11 21:33:41 -0500

    adding guac

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ const config = {
     slackResultsChannel: process.env.slack_results_channel,
     slackToken: process.env.slack_token,
     saveMeta: process.env.SAVE_META || false,
+    resetBets: false,
     db: {
-      conn: 'mongodb://localhost:27017',
+      conn: `mongodb+srv://${ process.env.db_username }:${ process.env.db_password }@cluster0-ltj7y.mongodb.net/test?retryWrites=true&w=majority`,
       name: 'vandelay-industries'
     }
   },
@@ -12,6 +13,7 @@ const config = {
     slackResultsChannel: process.env.slack_results_channel,
     slackToken: process.env.slack_token,
     saveMeta: true,
+    resetBets: true,
     db: {
       conn: `mongodb+srv://${ process.env.db_username }:${ process.env.db_password }@cluster0-ltj7y.mongodb.net/test?retryWrites=true&w=majority`,
       name: 'vandelay-industries'
